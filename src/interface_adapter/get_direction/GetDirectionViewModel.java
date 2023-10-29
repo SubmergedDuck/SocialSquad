89102762f432d9e4bd287ee59382b678c5e5ee98
SubmergedDuck <utamiwidowati1@gmail.com>
2023-10-29 15:24:51 -0400

    viewmod upd

--- src/interface_adapter/get_direction/GetDirectionViewModel.java
+++ src/interface_adapter/get_direction/GetDirectionViewModel.java
@@ -2,5 +2,20 @@ package interface_adapter.get_direction;
 
 import interface_adapter.ViewModel;
 
+import java.beans.PropertyChangeListener;
+
 public class GetDirectionViewModel extends ViewModel {
+    public GetDirectionViewModel(String viewName) {
+        super(viewName);
+    }
+
+    @Override
+    public void firePropertyChanged() {
+
+    }
+
+    @Override
+    public void addPropertyChangeListener(PropertyChangeListener listener) {
+
+    }
 }
--- src/interface_adapter/join_event/JoinEventViewModel.java
+++ src/interface_adapter/join_event/JoinEventViewModel.java
@@ -2,5 +2,20 @@ package interface_adapter.join_event;
 
 import interface_adapter.ViewModel;
 
+import java.beans.PropertyChangeListener;
+
 public class JoinEventViewModel extends ViewModel {
+    public JoinEventViewModel(String viewName) {
+        super(viewName);
+    }
+
+    @Override
+    public void firePropertyChanged() {
+
+    }
+
+    @Override
+    public void addPropertyChangeListener(PropertyChangeListener listener) {
+
+    }
 }
