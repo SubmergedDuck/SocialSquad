2f02c03959f234e9cb077de957ea216191ef9539
mikhaelorteza <mkorteza@gmail.com>
2023-10-29 16:22:31 -0400

    implemented signuppresenter

--- src/interface_adapter/signup/SignupPresenter.java
+++ src/interface_adapter/signup/SignupPresenter.java
@@ -1,4 +1,40 @@
 package interface_adapter.signup;
 
-public class SignupPresenter {
+
+import interface_adapter.login.LoginState;
+import interface_adapter.ViewManagerModel;
+import interface_adapter.login.LoginViewModel;
+import use_case.signup.SignupOutputBoundary;
+import use_case.signup.SignupOutputData;
+
+public class SignupPresenter implements SignupOutputBoundary {
+
+    private final SignupViewModel signupViewModel;
+
+    private final LoginViewModel loginViewModel;
+
+    private ViewManagerModel viewManagerModel;
+
+    public SignupPresenter(ViewManagerModel viewManagerModel, SignupViewModel signupViewModel, LoginViewModel loginViewModel){
+        this.signupViewModel = signupViewModel;
+        this.loginViewModel = loginViewModel;
+        this.viewManagerModel = viewManagerModel;
+    }
+
+    @Override
+    public void prepareFailView(String error) {
+        SignupState signupState = signupViewModel.getState();
+        signupState.setSignUpError(error);
+        signupViewModel.firePropertyChanged();
+    }
+
+    @Override
+    public void prepareSuccessView(SignupOutputData output) {
+        LoginState loginState = loginViewModel.getState();
+        loginState.setUsername(output.getUsername());
+        this.loginViewModel.setState(loginState);
+        loginViewModel.firePropertyChanged();
+        viewManagerModel.setActiveView(loginViewModel.getViewName());
+        viewManagerModel.firePropertyChanged();
+    }
 }
