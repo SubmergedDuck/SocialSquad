42814cc23ff28204c4c5045b79ec06bf6cb48b6c
mikhaelorteza <mkorteza@gmail.com>
2023-10-29 14:33:16 -0400

    signup controller and signupinputdata implemented

--- src/use_case/signup/SignupInputData.java
+++ src/use_case/signup/SignupInputData.java
@@ -1,4 +1,24 @@
 package use_case.signup;
 
 public class SignupInputData {
+    final private String username;
+    final private String realName;
+
+    final private String password;
+
+    final private String repeatPassword;
+
+    final private int age;
+
+    final private char sex;
+
+    public SignupInputData(String username, String realName, String password, String repeatPassword, int age, char sex){
+        this.username = username;
+        this.realName = realName;
+        this.password = password;
+        this.repeatPassword = repeatPassword;
+        this.age = age;
+        this.sex = sex;
+    }
+
 }
