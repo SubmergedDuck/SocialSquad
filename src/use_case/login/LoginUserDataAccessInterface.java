9c3538ca117c9839a031eb1243c5bef5a06f1cf1
Michelle Ma <michelle24ma04@gmail.com>
2023-10-30 14:10:05 -0400

    login related

--- src/use_case/login/LoginUserDataAccessInterface.java
+++ src/use_case/login/LoginUserDataAccessInterface.java
@@ -1,4 +1,11 @@
 package use_case.login;
 
+import entity.Users.CommonUser;
+
 public interface LoginUserDataAccessInterface {
+    boolean existsByName(String identifier);
+
+    void save(CommonUser commonUser);
+
+    CommonUser get(String username);
 }
