dbb2ea8797e5c99eba12be218c9da466501daa65
katrinasha <katrina.sha@mail.utoronto.ca>
2023-11-06 16:31:12 -0500

    implemented SearchEventInputData

--- src/use_case/search_event/SearchEventInputData.java
+++ src/use_case/search_event/SearchEventInputData.java
@@ -1,4 +1,35 @@
 package use_case.search_event;
 
+import entity.Location.Location;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 public class SearchEventInputData {
+    final private String eventName;
+    final private Location location;
+    final private LocalDateTime startTime;
+    final private LocalDateTime endTime;
+    final private String type;
+
+
+    public SearchEventInputData(String eventName, Location location, LocalDateTime startTime, LocalDateTime endTime, String type) {
+        this.eventName = eventName;
+        this.location = location;
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.type = type;
+    }
+
+    public SearchEventInputData(String eventName, Location location, LocalDateTime today, String type) {
+        /*
+
+        When user didn't specify a range of time to search events, will default it to within 24 hours
+         */
+        this.eventName = eventName;
+        this.location = location;
+        this.startTime = LocalDateTime.of(today.getYear(), today.getMonth(), today.getDayOfMonth(), 0, 0);
+        this.endTime = today.plusDays(1);
+        this.type = type;
+    }
 }
