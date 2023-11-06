72f9efbbbbaecd8e7aeca097bec3b575d5b66ef5
katrinasha <katrina.sha@mail.utoronto.ca>
2023-11-06 16:36:41 -0500

    implemented SearchEventOutputData

--- src/use_case/search_event/SearchEventOutputData.java
+++ src/use_case/search_event/SearchEventOutputData.java
@@ -1,4 +1,19 @@
 package use_case.search_event;
 
+import entity.Events.Event;
+
+import java.util.ArrayList;
+
 public class SearchEventOutputData {
+    private final boolean eventsExist;
+    private final ArrayList<Event> eligibleEvents;
+
+    public SearchEventOutputData (boolean eventsExist, ArrayList<Event> eligibleEvents){
+        this.eventsExist = eventsExist;
+        this.eligibleEvents = eligibleEvents;
+    }
+
+    public ArrayList<Event> getEligibleEvents(){
+        return this.eligibleEvents;
+    }
 }
