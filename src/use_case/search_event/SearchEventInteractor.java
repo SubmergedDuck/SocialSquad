4b8e14554339a927026fde185f39a6925abe84dc
katrinasha <katrina.sha@mail.utoronto.ca>
2023-11-06 17:05:15 -0500

    started the interactor

--- src/use_case/search_event/SearchEventInteractor.java
+++ src/use_case/search_event/SearchEventInteractor.java
@@ -1,4 +1,8 @@
 package use_case.search_event;
 
-public class SearchEventInteractor {
+public class SearchEventInteractor implements SearchEventInputBoundary {
+    @Override
+    public void execute(SearchEventInputData inputData) {
+        ;
+    }
 }
