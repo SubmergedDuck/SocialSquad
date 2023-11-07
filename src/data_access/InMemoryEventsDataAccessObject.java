3859ccac4eeac730d48897bade454bceb031e804
katrinasha <katrina.sha@mail.utoronto.ca>
2023-11-07 17:51:30 -0500

    Removed unnecessary methods from SearchEventDataAccessInterface and implemented InMemoryEventsDataAccessObject

--- src/data_access/InMemoryEventsDataAccessObject.java
+++ src/data_access/InMemoryEventsDataAccessObject.java
@@ -1,65 +1,79 @@
 package data_access;
 
-import entity.Location.CommonLocation;
+import entity.Events.Event;
 import entity.Location.Location;
+import use_case.create_event.CreateEventDataAccessInterface;
+import use_case.get_direction.GetDirectionDataAccessInterface;
+import use_case.join_event.JoinEventDataAccessInterface;
 import use_case.search_event.SearchEventDataAccessInterface;
-import use_case.search_event.SearchEventInteractor;
-
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
-public class InMemoryEventsDataAccessObject implements SearchEventDataAccessInterface {
-    SearchEventInteractor interactor;
+public class InMemoryEventsDataAccessObject implements GetDirectionDataAccessInterface,
+        CreateEventDataAccessInterface,
+        SearchEventDataAccessInterface,
+        JoinEventDataAccessInterface {
+    /**
+     * This is an in-memory event DAO to allow testing with the relevant interactors.
+     * @param
+     * @return
+     */
+    private final Map<Event, Integer> EventstoID = new HashMap<>();
+
+    public void save(Event event){
+        EventstoID.put(event, event.getEventID());
+    }
+
+    private boolean withinRange(Event event, LocalDateTime start, LocalDateTime end) {
+        return start.isAfter(event.getTime()) && end.isBefore(event.getTime());
+    }
 
     @Override
-    public boolean existByName(String name) {
-        if (name.equals("name")){
-            return true;
+    public ArrayList<Integer> returnByName(String name) {
+        /**
+         * Return an array list of eventIDs associated with events that match with the search by name
+         */
+        ArrayList<Integer> eligibleEvents = new ArrayList<>();
+        for (Event event: EventstoID.keySet()) {
+            if (event.getEventName().equals(name)){
+                eligibleEvents.add(EventstoID.get(event));
+            }
         }
-        return false;
+        return eligibleEvents;
     }
 
     @Override
-    public boolean existByLocation(Location location) {
-        if (location.equals(new CommonLocation(new ArrayList<>(), "address", "country"))) {
-            return true;
+    public ArrayList<Integer> returnByLocation(Location location) {
+        ArrayList<Integer> eligibleEvents = new ArrayList<>();
+        for (Event event: EventstoID.keySet()) {
+            if (event.getLocation().equals(location)){
+                eligibleEvents.add(EventstoID.get(event));
+            }
         }
-        return false;
+        return eligibleEvents;
     }
 
     @Override
-    public boolean existByType(String type) {
-        if (type.equals("type")){
-            return true;
+    public ArrayList<Integer> returnByTime(LocalDateTime start, LocalDateTime end) {
+        ArrayList<Integer> eligibleEvents = new ArrayList<>();
+        for (Event event: EventstoID.keySet()) {
+            if (withinRange(event, start, end)){
+                eligibleEvents.add(EventstoID.get(event));
+            }
         }
-        return false;
+        return eligibleEvents;
     }
 
     @Override
-    public boolean existByTime(LocalDateTime start, LocalDateTime end) {
-        if (start.isEqual(start) && end.equals(end)) {
-            return true;
+    public ArrayList<Integer> returnByType(String type) {
+        ArrayList<Integer> eligibleEvents = new ArrayList<>();
+        for (Event event: EventstoID.keySet()) {
+            if (event.getType().equals(type)){
+                eligibleEvents.add(EventstoID.get(event));
+            }
         }
-        return false;
-    }
-
-    @Override
-    public Integer returnByName(String name) {
-        return 1;
-    }
-
-    @Override
-    public Integer returnByLocation(Location location) {
-        return 1;
-    }
-
-    @Override
-    public Integer returnByTime(LocalDateTime start, LocalDateTime end) {
-        return 1;
-    }
-
-    @Override
-    public Integer returnByType(String type) {
-        return 1;
+        return eligibleEvents;
     }
 }
--- src/use_case/search_event/SearchEventDataAccessInterface.java
+++ src/use_case/search_event/SearchEventDataAccessInterface.java
@@ -5,10 +5,6 @@ import entity.Location.Location;
 import java.time.LocalDateTime;
 
 public interface SearchEventDataAccessInterface {
-    boolean existByName(String name);
-    boolean existByLocation(Location location);
-    boolean existByType(String type);
-    boolean existByTime(LocalDateTime start, LocalDateTime end);
     Integer returnByName(String name);
     Integer returnByLocation(Location location);
     Integer returnByTime(LocalDateTime start, LocalDateTime end);
