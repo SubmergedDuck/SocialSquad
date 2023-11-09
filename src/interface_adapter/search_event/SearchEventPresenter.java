033e36c282b97209c357fd274ba3795a597b1886
katrinasha <katrina.sha@mail.utoronto.ca>
2023-11-09 17:26:47 -0500

    Implemented SearchEventViewModel and SearchEventPresenter.

--- src/interface_adapter/search_event/SearchEventPresenter.java
+++ src/interface_adapter/search_event/SearchEventPresenter.java
@@ -1,4 +1,40 @@
 package interface_adapter.search_event;
 
-public class SearchEventPresenter {
+import interface_adapter.ViewManagerModel;
+import interface_adapter.ViewModel;
+import use_case.search_event.SearchEventOutputBoundary;
+import use_case.search_event.SearchEventOutputData;
+
+public class SearchEventPresenter implements SearchEventOutputBoundary {
+    public final SearchEventViewModel searchEventViewModel;
+    public final ShowEventViewModel showEventViewModel;
+
+    private ViewManagerModel viewManagerModel;
+    public SearchEventPresenter(ViewManagerModel viewManagerModel,
+                                SearchEventViewModel searchEventViewModel,
+                                ShowEventViewModel showEventViewModel) {
+        this.viewManagerModel = viewManagerModel;
+        this.searchEventViewModel = searchEventViewModel;
+        this.showEventViewModel = showEventViewModel;
+    }
+    @Override
+    public void prepareSuccessView(SearchEventOutputData outputData) {
+        // ON success, switch to show event state that presents all search results
+        ShowEventState showEventState = showEventViewModel.getState();
+        showEventState.setEventDisplayed(outputData.getCompleteMatch(), outputData.getPartialMatch());
+        this.showEventViewModel.setState(showEventState);
+        showEventViewModel.firePropertyChanged();
+
+        viewManagerModel.setActiveView(showEventViewModel.getViewName());
+        viewManagerModel.firePropertyChanged();
+
+    }
+
+    @Override
+    public void prepareFailView() {
+        SearchEventState searchEventState = searchEventViewModel.getState();
+        searchEventState.setNoEventFound();
+        searchEventViewModel.firePropertyChanged();
+
+    }
 }
