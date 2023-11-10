d626a8f8584b5377e0f4bb1493d3d971a8074a84
katrinasha <katrina.sha@mail.utoronto.ca>
2023-11-10 11:38:24 -0500

    Created a rough draft on SearchEventView, need modifications once Interface Adapters are implemented later

--- src/view/SearchEventView.java
+++ src/view/SearchEventView.java
@@ -10,47 +10,47 @@ import java.awt.event.ActionListener;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 
-//public class SearchEventView extends JPanel implements ActionListener, PropertyChangeListener {
-//    public final String viewName = "Search an event!";
-//
-//    private final SearchEventViewModel searchEventViewModel;
-//    private final JTextField searchRequestInputField;
-//    private final SearchEventController searchController;
-//
-//    private final JButton search;
-//    private final JButton back; //TODO: this back button will trigger the BackOut use case
-//
-//    public SearchEventView(SearchEventController searchController, SearchEventViewModel searchViewModel) {
-//        this.searchController = searchController;
-//        this.searchEventViewModel = searchViewModel;
-//
-//        JLabel title = new JLabel(SearchEventViewModel.TITLE_LABEL);
-//        title.setAlignmentX(Component.CENTER_ALIGNMENT);
-//
-//        LabelTextPanel searchRequest = new LabelTextPanel(new JLabel(SearchEventViewModel.SEARCH_LABEL), searchRequestInputField);
-//        JPanel buttons = new JPanel();
-//        search = new JButton(SearchEventViewModel.SEARCH_EVENT_BUTTON_LABEL);
-//        buttons.add(search);
-//
-//        search.addActionListener(
-//                new ActionListener() {
-//                    @Override
-//                    public void actionPerformed(ActionEvent e) {
-//                        if (e.getSource().equals(search)) {
-//                            //TODO: complete
-//                        }
-//                    }
-//                }
-//        );
-//    }
-//
-//    @Override
-//    public void actionPerformed(ActionEvent e) {
-//
-//    }
-//
-//    @Override
-//    public void propertyChange(PropertyChangeEvent evt) {
-//
-//    }
-//}
+public class SearchEventView extends JPanel implements ActionListener, PropertyChangeListener {
+    public final String viewName = "Search an event!";
+
+    private final SearchEventViewModel searchEventViewModel;
+    private final JTextField searchRequestInputField;
+    private final SearchEventController searchController;
+
+    private final JButton search;
+    private final JButton back; //TODO: this back button will trigger the BackOut use case
+
+    public SearchEventView(SearchEventController searchController, SearchEventViewModel searchViewModel) {
+        this.searchController = searchController;
+        this.searchEventViewModel = searchViewModel;
+
+        JLabel title = new JLabel(SearchEventViewModel.TITLE_LABEL);
+        title.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+        LabelTextPanel searchRequest = new LabelTextPanel(new JLabel(SearchEventViewModel.SEARCH_LABEL), searchRequestInputField);
+        JPanel buttons = new JPanel();
+        search = new JButton(SearchEventViewModel.SEARCH_EVENT_BUTTON_LABEL);
+        buttons.add(search);
+
+        search.addActionListener(
+                new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        if (e.getSource().equals(search)) {
+                            //TODO: complete
+                        }
+                    }
+                }
+        );
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+
+    @Override
+    public void propertyChange(PropertyChangeEvent evt) {
+
+    }
+}
