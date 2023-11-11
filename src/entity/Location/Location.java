9ca52772ef3f6001c1e29a15cd8f44eb36b31aae
mikhaelorteza <mkorteza@gmail.com>
2023-11-11 12:11:00 -0500

    updated types

--- src/entity/Location/Location.java
+++ src/entity/Location/Location.java
@@ -5,7 +5,7 @@ import java.util.List;
 
 public interface Location {
 
-        List getCoordinates();
+        String[] getCoordinates();
         String getAddress();
         String getCountry();
 
