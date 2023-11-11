42f5badbd57435c8e76e6e230c2b1acf63fa95f4
mikhaelorteza <mkorteza@gmail.com>
2023-11-11 14:07:42 -0500

    unrelated files

--- src/use_case/create_event/CreateEventOutputBoundary.java
+++
@@ -1,7 +0,0 @@
-package use_case.create_event;
-
-
-public interface CreateEventOutputBoundary {
-    void prepareFailView(String error); //Pop up message will occur with the error
-    void prepareSuccessView(String message); //Not set in stone, but perhaps a popup message then exit out of create event view.
-}
