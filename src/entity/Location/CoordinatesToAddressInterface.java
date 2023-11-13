369d10f4ddcfe2c04b4b42d36feca4d2659c628d
mikhaelorteza <mkorteza@gmail.com>
2023-11-13 14:49:52 -0500

    unrelated files

--- src/entity/Location/CoordinatesToAddressInterface.java
+++
@@ -1,9 +0,0 @@
-package entity.Location;
-
-import java.io.IOException;
-
-public interface CoordinatesToAddressInterface {
-    String getAddress() throws IOException;
-
-    String getCountry() throws IOException;
-}
