865fe3ac7036b5a4e2fb1c94e5aa72b94e3f55ee
Mikhael Orteza <mkorteza@gmail.com>
2023-11-20 13:52:44 -0500

    updated in memory daos to account for create event

--- src/data_access/InMemoryUsersDataAccessObject.java
+++ src/data_access/InMemoryUsersDataAccessObject.java
@@ -2,6 +2,7 @@ package data_access;
 
 import entity.Events.Event;
 import entity.Users.User;
+import use_case.create_event.CreateEventDataAccessInterface;
 import use_case.join_event.JoinEventDataAccessInterface;
 import use_case.remove_participant.RemoveParticipantDataAccessInterface;
 import use_case.search_event.SearchEventDataAccessInterface;
@@ -12,7 +13,8 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class InMemoryUsersDataAccessObject implements
-        SearchEventDataAccessInterface, RemoveParticipantDataAccessInterface, SignupUserDataAccessInterface {
+        SearchEventDataAccessInterface, RemoveParticipantDataAccessInterface, SignupUserDataAccessInterface,
+        CreateEventDataAccessInterface {
 
     private final HashMap<String, User> usernameToUser = new HashMap();
 
@@ -38,6 +40,9 @@ public class InMemoryUsersDataAccessObject implements
     public void save(User user){
         usernameToUser.put(user.getUsername(), user);
     }
+    public User getUser(String username){
+        return usernameToUser.get(username);
+    }
     @Override
     public ArrayList<Event> getFullMatchEvents(SearchEventInputData inputData) {
         return null;
@@ -46,6 +51,19 @@ public class InMemoryUsersDataAccessObject implements
     @Override
     public ArrayList<Event> getPartialMatchEvents(SearchEventInputData inputData) {
         return null;
+    }
+
+    @Override
+    public Integer generateEventID() {
+        return null;
+    }
+
+    @Override
+    public void save(Event event) {
+        String ownerUser = event.getOwnerUser();
+        User eventOwner = this.usernameToUser.get(ownerUser);
+        ArrayList<Event> hostedEvents = eventOwner.getCreatedEvents();
+        hostedEvents.add(event);
     }
 }
 
