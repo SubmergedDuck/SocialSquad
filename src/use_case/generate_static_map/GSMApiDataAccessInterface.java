2f2860e8953004aca3281860adba085c36e8ad09
Mikhael Orteza <mkorteza@gmail.com>
2023-11-21 20:31:07 -0500

    save

--- src/use_case/generate_static_map/GSMApiDataAccessInterface.java
+++ src/use_case/generate_static_map/GSMApiDataAccessInterface.java
@@ -1,11 +1,12 @@
 package use_case.generate_static_map;
 
 import java.awt.image.BufferedImage;
+import java.io.IOException;
 import java.util.Map;
 
 /*
 https://dev.virtualearth.net/REST/v1/Imagery/Map/AerialWithLabels?pp=40.804000,-74.464460;;1&pp=40.815180,-74.219250;;2&pp=40.881210,-74.168020;;3&pp=40.810830,-74.260250;;4&pp=47.64054,-122.12934;;5&dcl=1&key=An_Fn1bhTQPuLmUi8MsH-5_btdPIKgINhoecH-ayEq0rjUhrnFbXoHHVnwjAli_K
  */
 public interface GSMApiDataAccessInterface {
-    BufferedImage generateMap(String userCoordinate, Map<Integer, String> eventCoordinates);
+    BufferedImage generateMap(String userCoordinate, Map<Integer, String> eventCoordinates) throws IOException;
 }
