804332af5b3843686f2c30e00bd1939266179b1c
Mikhael Orteza <mkorteza@gmail.com>
2023-11-22 10:57:52 -0500

    changed input boundary

--- src/use_case/generate_static_map/GSMInputBoundary.java
+++ src/use_case/generate_static_map/GSMInputBoundary.java
@@ -1,5 +1,7 @@
 package use_case.generate_static_map;
 
+import java.io.IOException;
+
 public interface GSMInputBoundary {
-    void execute(GSMInputData inputData);
+    void execute(GSMInputData inputData) throws IOException;
 }
