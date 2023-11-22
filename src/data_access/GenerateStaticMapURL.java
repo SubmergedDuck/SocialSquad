3604cbb3eeb25bb9cd0144ee772ec3259592bc24
Mikhael Orteza <mkorteza@gmail.com>
2023-11-22 10:35:41 -0500

    generate static map url fully implemented

--- src/data_access/GenerateStaticMapURL.java
+++ src/data_access/GenerateStaticMapURL.java
@@ -10,19 +10,22 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.Map;
 
-/*
-https://dev.virtualearth.net/REST/v1/Imagery/Map/AerialWithLabels?pp=40.804000,-74.464460;;1&pp=40.815180,-74.219250;;2&pp=40.881210,-74.168020;;3&pp=40.810830,-74.260250;;4&pp=47.64054,-122.12934;;5&dcl=1&key=An_Fn1bhTQPuLmUi8MsH-5_btdPIKgINhoecH-ayEq0rjUhrnFbXoHHVnwjAli_K
- */
 public class GenerateStaticMapURL implements GSMApiDataAccessInterface {
+    private final int maxPins = 18; //Maximum pin amount allowed for an HTTP GET method.
     private final String api_key = "An_Fn1bhTQPuLmUi8MsH-5_btdPIKgINhoecH-ayEq0rjUhrnFbXoHHVnwjAli_K";
+
     @Override
     public BufferedImage generateMap(String userCoordinate, Map<Integer, String> eventCoordinates) throws IOException {
         try {
             String currentLink = "https://dev.virtualearth.net/REST/v1/Imagery/Map/AerialWithLabels?";
+            String userParameter = String.format("pp=%s&", userCoordinate);
+            currentLink = currentLink + userParameter;
             for (Integer pinNum : eventCoordinates.keySet()) {
-                String coordinates = eventCoordinates.get(pinNum);
-                String addedParameter = String.format("pp=%s;;%d&", coordinates, pinNum);
-                currentLink = currentLink + addedParameter;
+                if (pinNum < maxPins - 1) { //Want a maximum of 17 pinpoints for events. The remaining one will go to the user.
+                    String coordinates = eventCoordinates.get(pinNum);
+                    String addedParameter = String.format("pp=%s;;%d&", coordinates, pinNum);
+                    currentLink = currentLink + addedParameter;
+                }
             }
             String finalPart = String.format("dcl=1&key=%s", api_key);
             currentLink = currentLink + finalPart;
@@ -30,13 +33,13 @@ public class GenerateStaticMapURL implements GSMApiDataAccessInterface {
             HttpURLConnection connection = (HttpURLConnection) url.openConnection();
             connection.setRequestMethod("GET");
             int responseCode = connection.getResponseCode();
-
-            if (responseCode == 200){
+            if (responseCode == 200) {
                 InputStream inputStream = connection.getInputStream();
                 BufferedImage image = ImageIO.read(inputStream);
                 return image;
             }
-        } catch (IOException e){
+        } catch (IOException e) {
+            //If null is returned, the view model needs to appropriately deal with this situation and tell the user that something went wrong.
             return null;
         }
         return null;
