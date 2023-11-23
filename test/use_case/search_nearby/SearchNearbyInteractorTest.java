a69f17477bcbb9383483846e47c812f97d0d22ce
katrinasha <katrina.sha@mail.utoronto.ca>
2023-11-22 23:35:44 -0500

    Test passed.

--- test/use_case/search_nearby/SearchNearbyInteractorTest.java
+++ test/use_case/search_nearby/SearchNearbyInteractorTest.java
@@ -22,23 +22,27 @@ class SearchNearbyInteractorTest {
     void searchSuccess() {
         CommonLocationFactory factory = new CommonLocationFactory();
         try {
-            Location location = factory.makeLocation("43.665510,-79.387280"); // Home
-            Location location2 = factory.makeLocation("43.645531,-79.380348"); // Union Station (3KM)
-            Event event = new CommonEvent(1, "badminton", "owner", location, new ArrayList<>(), new ArrayList<>(), LocalDateTime.now(), "type", "description", false, 10);
-            Event event2 = new CommonEvent(2, "group trip", "owner", location2, new ArrayList<>(), new ArrayList<>(), LocalDateTime.now(), "type", "description", false, 10);
+            Location location = factory.makeLocation("(43.665510,-79.387280)"); // Home, within 2KM
+            Location location2 = factory.makeLocation("(43.645531,-79.380348)"); // Union Station (3KM)
+            Event event = new CommonEvent(1, "badminton", "owner", location, new ArrayList<>(),
+                    new ArrayList<>(), LocalDateTime.now(), "type", "description", false,
+                    10); // This event should be returned
+            Event event2 = new CommonEvent(2, "group trip", "owner", location2, new ArrayList<>(),
+                    new ArrayList<>(), LocalDateTime.now(), "type", "description", false,
+                    10); // This event should not be returned
 
             SearchNearbyDataAccessInterface inmemoryEventDAO = new InMemoryEventsDataAccessObject();
             ((InMemoryEventsDataAccessObject) inmemoryEventDAO).save(event);
             ((InMemoryEventsDataAccessObject) inmemoryEventDAO).save(event2);
-            SearchNearbyInputData inputData = new SearchNearbyInputData(new String[]{"43.664219", "-79.398621"}); // MP building
+            SearchNearbyInputData inputData = new SearchNearbyInputData(new String[]{"43.664219", "-79.398621"}); // User request from MP building
 
-            SearchNearbyOutputBoundary sucessPresenter = new SearchNearbyOutputBoundary() {
+
+            SearchNearbyOutputBoundary successPresenter = new SearchNearbyOutputBoundary() {
                 @Override
                 public void prepareSuccessView(SearchNearbyOutputData outputData) {
                     assert !outputData.usecaseFailed;
-                    assert outputData.getEvents().size() == 2;
+                    assert outputData.getEvents().size() == 1;
                     assert outputData.getEvents().get(0) == event;
-                    assert outputData.getEvents().get(1) == event2;
                 }
 
                 @Override
@@ -47,7 +51,9 @@ class SearchNearbyInteractorTest {
 
                 }
             };
-        } catch (IOException e) {
+            SearchNearbyInteractor interactor = new SearchNearbyInteractor(inmemoryEventDAO, successPresenter);
+            interactor.execute(inputData);
+        } catch (Exception e) {
             throw new RuntimeException(e);
         }
 
@@ -58,7 +64,7 @@ class SearchNearbyInteractorTest {
     void searchFailed() {
         CommonLocationFactory factory = new CommonLocationFactory();
         try {
-            Location location2 = factory.makeLocation("43.645531,-79.380348"); // Union Station (3KM)
+            Location location2 = factory.makeLocation("(43.645531,-79.380348)"); // Union Station (3KM)
             Event event2 = new CommonEvent(2, "group trip", "owner", location2, new ArrayList<>(), new ArrayList<>(), LocalDateTime.now(), "type", "description", false, 10);
             SearchNearbyDataAccessInterface inmemoryEventDAO = new InMemoryEventsDataAccessObject();
             ((InMemoryEventsDataAccessObject) inmemoryEventDAO).save(event2);
