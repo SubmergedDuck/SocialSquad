06b047f5394c56a115f477a2634606965b8e2ac3
Mikhael Orteza <mkorteza@gmail.com>
2023-11-23 16:27:35 -0500

    added image height and width as input data.

--- src/use_case/generate_static_map/GSMInputData.java
+++ src/use_case/generate_static_map/GSMInputData.java
@@ -3,11 +3,17 @@ package use_case.generate_static_map;
 public class GSMInputData {
     private final String username;
     private final int totalPins;
+    private final float width;
+    private final float height;
 
-    public GSMInputData(String username, int totalPins){
+    public GSMInputData(String username, int totalPins, float width, float height){
         this.username = username;
         this.totalPins = totalPins;
+        this.width = width;
+        this.height = height;
     }
     String getUsername(){return this.username;}
     int getTotalPins(){return this.totalPins;}
+    float getWidth(){return this.width;}
+    float getHeight(){return this.height;}
 }
