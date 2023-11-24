d071be3deaa70d8cd30894d3cee492528c60e897
katrinasha <katrina.sha@mail.utoronto.ca>
2023-11-23 23:19:20 -0500

    Implemented State and ViewModel.

--- src/interface_adapter/join_event/JoinEventState.java
+++ src/interface_adapter/join_event/JoinEventState.java
@@ -1,4 +1,23 @@
 package interface_adapter.join_event;
 
 public class JoinEventState {
+    private boolean success = false;
+    private String error = "";
+
+    public JoinEventState(JoinEventState copy) {
+        success = copy.success;
+        error = copy.error;
+    }
+
+    public JoinEventState(){
+
+    }
+
+    public void setSuccess(boolean success) {
+        this.success = success;
+    }
+
+    public void setError(String failureReason) {
+        this.error = failureReason;
+    }
 }
