c65e077c25045fb620137f31b825833d04f64180
katrinasha <katrina.sha@mail.utoronto.ca>
2023-11-23 23:18:40 -0500

    Defined output data and output boundary for the presenter to work with.

--- src/use_case/join_event/JoinEventOutputData.java
+++ src/use_case/join_event/JoinEventOutputData.java
@@ -1,4 +1,17 @@
 package use_case.join_event;
 
+import interface_adapter.join_event.JoinEventState;
+
 public class JoinEventOutputData {
+    public final boolean usecaseSuccess = false; // Output Data is only needed when the use case failed
+    private final String failureReason;
+
+    public JoinEventOutputData(String failureReason) {
+        this.failureReason = failureReason;
+    }
+
+    public String getFailureReason() {
+        return failureReason;
+    }
+
 }
