6c00e5902f6f08a3802918f7bb31cea5fe92306c
SubmergedDuck <utamiwidowati1@gmail.com>
2023-11-25 02:10:33 -0500

    JoinEventOutputData Completed

--- src/use_case/join_event/JoinEventOutputData.java
+++ src/use_case/join_event/JoinEventOutputData.java
@@ -1,4 +1,27 @@
 package use_case.join_event;
 
+/**
+ * Output data for the join event use case.
+ */
+
 public class JoinEventOutputData {
+    private final String capacity;
+
+    /**
+     * Constructor for JoinEventOutputData.
+     * @param capacity the capacity of the event
+     */
+
+    public JoinEventOutputData(String capacity) {
+        this.capacity = capacity;
+    }
+
+    /**
+     * Getter for the capacity.
+     * @return the capacity
+     */
+
+    public String getCapacity() {
+        return this.capacity;
+    }
 }
