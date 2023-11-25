291352cbadb2cbaca6dc16d5bc440b94597d2db4
SubmergedDuck <utamiwidowati1@gmail.com>
2023-11-25 08:58:19 -0500

    LeaveEvent Use Case Completed, Tests Still Needed

--- src/use_case/leave_event/LeaveEventEventDataAccessInterface.java
+++ src/use_case/leave_event/LeaveEventEventDataAccessInterface.java
@@ -1,5 +1,7 @@
 package use_case.leave_event;
 
+import java.util.ArrayList;
+
 /**
  * The event data access interface for the leave event use case.
  */
@@ -9,4 +11,10 @@ public interface LeaveEventEventDataAccessInterface {
      * This method will use the event DAO to look up the event and remove the user from the peopleJoined list.
      */
     void userLeaveEvent(String username, Integer eventID);
+
+    /**
+     * This method will use the event DAO to look up the event and return getPeopleJoined.
+     */
+    ArrayList<String> getPeopleJoined(Integer eventID);
+
 }
--- src/use_case/leave_event/LeaveEventInteractor.java
+++ src/use_case/leave_event/LeaveEventInteractor.java
@@ -1,4 +1,59 @@
 package use_case.leave_event;
 
+import use_case.leave_event.LeaveEventUserDataAccessInterface;
+import use_case.leave_event.LeaveEventEventDataAccessInterface;
+import use_case.leave_event.LeaveEventOutputBoundary;
+
+import java.util.ArrayList;
+
+/**
+ * The interactor for the leave event use case.
+ */
+
 public class LeaveEventInteractor {
+    private final LeaveEventOutputBoundary leaveEventPresenter;
+    private final LeaveEventUserDataAccessInterface userDataAccessObject;
+    private final LeaveEventEventDataAccessInterface eventsDataAccessObject;
+
+    /**
+     * Constructor for LeaveEventInteractor.
+     *
+     * @param leaveEventPresenter    the presenter for the leave event use case
+     * @param userDataAccessObject   the user data access object for the leave event use case
+     * @param eventsDataAccessObject the event data access object for the leave event use case
+     */
+    public LeaveEventInteractor(LeaveEventOutputBoundary leaveEventPresenter,
+                                LeaveEventUserDataAccessInterface userDataAccessObject,
+                                LeaveEventEventDataAccessInterface eventsDataAccessObject) {
+        this.leaveEventPresenter = leaveEventPresenter;
+        this.userDataAccessObject = userDataAccessObject;
+        this.eventsDataAccessObject = eventsDataAccessObject;
+    }
+
+    /**
+     * Executes the leave event use case.
+     *
+     * @param leaveEventInputData The input data for the use case.
+     *                            Removes the event from the user's joinedEvents list and removes the user from the event's peopleJoined list.
+     */
+
+    public void execute(LeaveEventInputData leaveEventInputData) {
+        // Remove user from event in both DAOs
+        userDataAccessObject.userLeaveEvent(leaveEventInputData.getUsername(), leaveEventInputData.getEventID());
+        eventsDataAccessObject.userLeaveEvent(leaveEventInputData.getUsername(), leaveEventInputData.getEventID());
+
+        // Get the updated peopleJoined list from the event DAO
+        ArrayList<String> peopleJoined = eventsDataAccessObject.getPeopleJoined(leaveEventInputData.getEventID());
+        LeaveEventOutputData leaveEventOutputData = new LeaveEventOutputData(peopleJoined);
+
+        // Prepare the success view
+        leaveEventPresenter.prepareSuccessView(leaveEventOutputData);
+    }
+
 }
+
+
+
+
+
+
