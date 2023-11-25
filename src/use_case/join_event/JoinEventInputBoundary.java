82c3e31bd1b3959b134253b8fa92ee897c95bdc9
SubmergedDuck <utamiwidowati1@gmail.com>
2023-11-25 01:52:03 -0500

    JoinEvent Input Boundary Complete

--- src/use_case/join_event/JoinEventInputBoundary.java
+++ src/use_case/join_event/JoinEventInputBoundary.java
@@ -1,4 +1,9 @@
 package use_case.join_event;
 
 public interface JoinEventInputBoundary {
+    /**
+     * Executes the join event use case.
+     * @param joinEventInputData The input data for the use case.
+     */
+    void execute(JoinEventInputData joinEventInputData);
 }
