4a9a8892d0f5eaa7edfbb6ae7bb9479660d39899
SubmergedDuck <utamiwidowati1@gmail.com>
2023-11-25 01:57:34 -0500

    JoinEventInputData Completed

--- src/use_case/join_event/JoinEventInputData.java
+++ src/use_case/join_event/JoinEventInputData.java
@@ -1,4 +1,32 @@
 package use_case.join_event;
 
+/**
+ * Input data for the join event use case.
+ */
 public class JoinEventInputData {
+    private final int eventID;
+    private final String username;
+
+    /**
+     * Constructor for JoinEventInputData.
+     * @param eventID the ID of the event that we are trying to have the user join
+     * @param username the username of the user that we are trying to have join the event
+     */
+    public JoinEventInputData(int eventID, String username){
+        this.eventID = eventID;
+        this.username = username;
+    }
+
+    /**
+     * Getter for the event ID.
+     * @return the event ID
+     */
+    int getEventID(){return this.eventID;}
+
+    /**
+     * Getter for the username.
+     * @return the username
+     */
+    String getUsername(){return this.username;}
+
 }
