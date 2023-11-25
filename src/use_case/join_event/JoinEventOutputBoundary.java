5b5a1587cf3abbd7dfda7bfc690c07ffe5948eed
SubmergedDuck <utamiwidowati1@gmail.com>
2023-11-25 02:06:52 -0500

    JoinEventOutputBoundary Completed

--- src/use_case/join_event/JoinEventOutputBoundary.java
+++ src/use_case/join_event/JoinEventOutputBoundary.java
@@ -1,4 +1,22 @@
 package use_case.join_event;
 
+import use_case.join_event.JoinEventOutputData;
+
+/**
+ * The output boundary interface for the JoinEvent use case, used by the interactor and presenter.
+ */
+
 public interface JoinEventOutputBoundary {
+    /**
+     * A public method that lets the presenter generates a success view after the interactor returning the search result,
+     * in the form of the outputData.
+     * @param outputData The result returned by the interactor, it contains updated event information (e.g. capacity).
+     */
+    public void prepareSuccessView(JoinEventOutputBoundary outputData);
+
+    /**
+     * A public method that lets the presenter generates a fail view after the interactor is unable to let user join the event.
+     * Case: Event is full (e.g. 8/8).
+     */
+    public void prepareFailView();
 }
