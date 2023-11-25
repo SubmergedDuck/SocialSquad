0fa860e95a0d643af98ffcbfa2d1889dbb55fd76
SubmergedDuck <utamiwidowati1@gmail.com>
2023-11-25 08:43:08 -0500

    LeaveEventInputData & OutputData Completed

--- src/use_case/leave_event/LeaveEventOutputData.java
+++ src/use_case/leave_event/LeaveEventOutputData.java
@@ -1,4 +1,25 @@
 package use_case.leave_event;
 
+import java.util.ArrayList;
+
+/**
+ * Output data for the leave event use case.
+ */
 public class LeaveEventOutputData {
+
+    private final ArrayList<String> getPeopleJoined;
+
+    /**
+     * Constructor for JoinEventOutputData.
+     * @param getPeopleJoined the people joined
+     */
+
+    public LeaveEventOutputData(ArrayList<String> getPeopleJoined) {
+            this.getPeopleJoined = getPeopleJoined;
+    }
 }
+
+
+
+
+
