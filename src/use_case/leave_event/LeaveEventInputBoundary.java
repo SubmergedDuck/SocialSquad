89172efdecec56f86cda7dbc0f0142cca5a21777
SubmergedDuck <utamiwidowati1@gmail.com>
2023-11-25 03:17:49 -0500

    LeaveEventInputBoundary Completed

--- src/use_case/leave_event/LeaveEventInputBoundary.java
+++ src/use_case/leave_event/LeaveEventInputBoundary.java
@@ -1,4 +1,15 @@
 package use_case.leave_event;
 
+/**
+ * Input boundary for the leave event use case.
+ */
 public interface LeaveEventInputBoundary {
+
+    /**
+     * Executes the join event use case.
+     * @param leaveEventInputData The input data for the use case.
+     */
+
+    void execute(LeaveEventInputData leaveEventInputData);
+
 }
