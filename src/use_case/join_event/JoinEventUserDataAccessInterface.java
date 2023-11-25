ce722c00f53b483af34893f85f4976fdb13cacd6
SubmergedDuck <utamiwidowati1@gmail.com>
2023-11-25 18:48:16 -0500

    Added JoinEventCurrentUserDAI

--- src/use_case/join_event/JoinEventUserDataAccessInterface.java
+++ src/use_case/join_event/JoinEventUserDataAccessInterface.java
@@ -3,7 +3,7 @@ package use_case.join_event;
 /**
  * The data access interface for the join event use case.
  */
-public interface JoinEventUserDataAccessInterface {
+public interface JoinEventCurrentUserDataAccessInterface {
     /**
     This method will use the user DAO to look up the user and add the given event in their joined events list.
     */
---
+++ test/use_case/join_event/JoinEventInteractorTest.java
@@ -0,0 +1,2 @@
+package use_case.join_event;public class JoinEventInteractorTest {
+}
