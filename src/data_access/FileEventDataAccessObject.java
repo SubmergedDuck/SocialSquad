ef029963d80c715c0aad07f111522168cbdc6cea
Mikhael Orteza <mkorteza@gmail.com>
2023-11-26 01:29:08 -0500

    file event dao completed

--- src/data_access/FileEventDataAccessObject.java
+++ src/data_access/FileEventDataAccessObject.java
@@ -1,17 +1,16 @@
 package data_access;
 
+import entity.Events.CommonEventFactory;
 import entity.Events.Event;
 import entity.Events.EventFactory;
+import entity.Location.CommonLocationFactory;
+import entity.Location.Location;
 import entity.Location.LocationFactory;
 
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.LinkedHashMap;
-import java.util.Map;
+import java.io.*;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
 
 
 public class FileEventDataAccessObject {
@@ -20,16 +19,60 @@ public class FileEventDataAccessObject {
     private final Map<Integer, Event> eventsToID = new HashMap<>();
     private final EventFactory eventFactory;
     private final LocationFactory locationFactory;
+    private final String elementSeperator = " ";
+    private final DateTimeFormatter formatter;
 
-    public FileEventDataAccessObject(String csvPath,EventFactory eventFactory, LocationFactory locationFactory){
+    public FileEventDataAccessObject(String csvPath,EventFactory eventFactory, LocationFactory locationFactory, DateTimeFormatter formatter) throws IOException {
         this.eventDatabase = new File(csvPath);
         this.eventFactory = eventFactory;
         this.locationFactory = locationFactory;
+        this.formatter = formatter;
 
         String[] labels = {"owner", "eventID", "eventName", "coordinates", "peopleJoined", "peopleWaitlisted",
                 "time", "type", "description", "privacy", "capacity"};
         for (int i = 0; i < labels.length; i++){
             headers.put(labels[i], i);
+        }
+
+        if (eventDatabase.length() == 0){
+            save();
+        } else {
+            try (BufferedReader reader = new BufferedReader(new FileReader(eventDatabase))){
+                String header = reader.readLine();
+
+                String row;
+                while ((row = reader.readLine()) != null){
+                    String[] eventValues = row.split(",");
+
+                    //Parsing values
+                    String ownerUser = eventValues[headers.get("owner")];
+                    Integer eventID = Integer.valueOf(eventValues[headers.get("eventID")]);
+                    String eventName = eventValues[headers.get("eventName")];
+                    String[] coordinates = eventValues[headers.get("coordinates")].split(elementSeperator);
+                    Location eventLocation = locationFactory.makeLocation(String.format("(%s,%s)", coordinates[0], coordinates[1]));
+                    String[] usernamesJoined = eventValues[headers.get("peopleJoined")].split(elementSeperator);
+                    ArrayList<String> participantsJoined = new ArrayList<>(Arrays.asList(usernamesJoined));
+                    String[] usernamesWaitlisted = eventValues[headers.get("peopleWaitlisted")].split(elementSeperator);
+                    ArrayList<String> participantsWaitlisted = new ArrayList<>(Arrays.asList(usernamesWaitlisted));
+                    LocalDateTime eventTime = LocalDateTime.parse(eventValues[headers.get("time")],formatter);
+                    String eventType = eventValues[headers.get("type")];
+                    String description = eventValues[headers.get("description")];
+                    boolean privacy = false;
+                    if (eventValues[headers.get("privacy")].toLowerCase().equals("true")){
+                        privacy = true;
+                    }
+                    Integer capacity = Integer.valueOf(eventValues[headers.get("capacity")]);
+
+                    //Creating the event object
+                    Event createdEvent = eventFactory.create(eventID,eventName,ownerUser,eventLocation,participantsJoined,
+                            participantsWaitlisted,eventTime,eventType,description,privacy,capacity);
+
+                    //Saving the created event to the arraylist
+                    eventsToID.put(createdEvent.getEventID(),createdEvent);
+                }
+            } catch (IOException e){
+                throw e;
+            }
         }
     }
 
@@ -45,9 +88,10 @@ public class FileEventDataAccessObject {
         try {
             writer = new BufferedWriter(new FileWriter(eventDatabase));
             writer.write(String.join(",", headers.keySet()));
+            writer.newLine();
             for (Event event : eventsToID.values()){
                 String[] coordinates = event.getLocation().getCoordinates();
-                String formattedCoordinates = String.format("(%s,%s)", coordinates[0], coordinates[1]);
+                String formattedCoordinates = String.format("(%s%s%s)", coordinates[0], elementSeperator, coordinates[1]);
                 String peopleJoined = formatStringList(event.getPeopleJoined());
                 String peopleWaitlisted = formatStringList(event.getPeopleWaitlisted());
                 String line = String.format("%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s", event.getOwnerUser(), event.getEventID(),
@@ -67,7 +111,7 @@ public class FileEventDataAccessObject {
             if (i == stringList.size() - 1){
                 currentString = currentString + stringList.get(i);
             } else {
-                currentString = currentString + stringList.get(i) + ",";
+                currentString = currentString + stringList.get(i) + elementSeperator;
             }
         }
         return currentString;
