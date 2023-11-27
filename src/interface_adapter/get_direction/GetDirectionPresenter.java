c7a148b02a972167b4c6d29393dc8bf70fc75900
Mikhael Orteza <mkorteza@gmail.com>
2023-11-27 13:50:22 -0500

    interface adapters implemented for get direction

--- src/interface_adapter/get_direction/GetDirectionPresenter.java
+++ src/interface_adapter/get_direction/GetDirectionPresenter.java
@@ -1,4 +1,26 @@
 package interface_adapter.get_direction;
 
-public class GetDirectionPresenter {
+import interface_adapter.ViewManagerModel;
+import use_case.get_direction.GetDirectionOutputBoundary;
+import use_case.get_direction.GetDirectionOutputData;
+
+public class GetDirectionPresenter implements GetDirectionOutputBoundary {
+
+    private final GetDirectionViewModel getDirectionViewModel;
+    private final ViewManagerModel viewManagerModel;
+
+    public GetDirectionPresenter(GetDirectionViewModel getDirectionViewModel, ViewManagerModel viewManagerModel){
+        this.getDirectionViewModel = getDirectionViewModel;
+        this.viewManagerModel = viewManagerModel;
+    }
+    @Override
+    public void prepareView(GetDirectionOutputData outputData) {
+        GetDirectionState state = getDirectionViewModel.getState();
+        state.setGeneratedImage(outputData.getDirectionImage());
+        this.getDirectionViewModel.setState(state);
+        getDirectionViewModel.firePropertyChanged();
+
+        viewManagerModel.setActiveView(getDirectionViewModel.getViewName());
+        viewManagerModel.firePropertyChanged();
+    }
 }
