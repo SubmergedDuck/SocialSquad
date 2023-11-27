cebf1bc2c093263c016af1f88481ead21a350f15
Mikhael Orteza <mkorteza@gmail.com>
2023-11-27 15:53:20 -0500

    saved changes

--- src/interface_adapter/generate_static_map/GenerateStaticMapPresenter.java
+++ src/interface_adapter/generate_static_map/GenerateStaticMapPresenter.java
@@ -1,4 +1,18 @@
 package interface_adapter.generate_static_map;
 
-public class GenerateStaticMapPresenter {
+import use_case.generate_static_map.GSMOutputBoundary;
+import use_case.generate_static_map.GSMOutputData;
+
+public class GenerateStaticMapPresenter implements GSMOutputBoundary {
+
+    private final GenerateStaticMapViewModel generateStaticMapViewModel;
+
+    public GenerateStaticMapPresenter(GenerateStaticMapViewModel generateStaticMapViewModel){
+        this.generateStaticMapViewModel = generateStaticMapViewModel;
+    }
+
+    @Override
+    public void prepareView(GSMOutputData outputData) {
+
+    }
 }
