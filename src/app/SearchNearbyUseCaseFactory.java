99f5f05bd013a802e742236c417726a2acd9ee9e
katrinasha <katrina.sha@mail.utoronto.ca>
2023-11-28 14:45:09 -0500

    Removed createSearchNearbyUseCase because the SearchNearby controller is not needed for this view.

--- src/app/SearchNearbyUseCaseFactory.java
+++ src/app/SearchNearbyUseCaseFactory.java
@@ -1,6 +1,8 @@
 package app;
 
 import interface_adapter.ViewManagerModel;
+import interface_adapter.back_out.BackOutController;
+import interface_adapter.get_event_details.GetEventDetailsController;
 import interface_adapter.search_nearby.SearchNearbyController;
 import interface_adapter.search_nearby.SearchNearbyPresenter;
 import interface_adapter.search_nearby.SearchNearbyViewModel;
@@ -15,9 +17,11 @@ public class SearchNearbyUseCaseFactory {
     private SearchNearbyUseCaseFactory() {} // Prevent instantiation
 
     public static SearchNearbyView create(ViewManagerModel viewManagerModel, SearchNearbyViewModel searchNearbyViewModel,
-                                          SearchNearbyDataAccessInterface searchNearbyDataAccessobject) {
-        SearchNearbyController searchNearbyController = createSearchNearbyUseCase(viewManagerModel, searchNearbyViewModel,
-                searchNearbyDataAccessobject);
+                                          SearchNearbyDataAccessInterface searchNearbyDataAccessobject,
+                                          GetEventDetailsController getEventDetailsController,
+                                          BackOutController backOutController) {
+//        SearchNearbyController searchNearbyController = createSearchNearbyUseCase(viewManagerModel, searchNearbyViewModel,
+//                searchNearbyDataAccessobject);
         return new SearchNearbyView(searchNearbyViewModel, getEventDetailsController, backOutController);
     }
 
