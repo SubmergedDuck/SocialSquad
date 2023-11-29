5e3ae453619bd8340a97ebd2f33cd022b775471d
katrinasha <katrina.sha@mail.utoronto.ca>
2023-11-28 20:15:28 -0500

    Implemented createView and createUseCase.

---
+++ src/app/GetEventDetailsUseCaseFactory.java
@@ -0,0 +1,31 @@
+package app;
+
+import interface_adapter.ViewManagerModel;
+import interface_adapter.ViewManagerModelAdapter;
+import interface_adapter.back_out.BackOutController;
+import interface_adapter.get_event_details.GetEventDetailsController;
+import interface_adapter.get_event_details.GetEventDetailsPresenter;
+import interface_adapter.get_event_details.GetEventDetailsViewModel;
+import interface_adapter.join_event.JoinEventController;
+import use_case.back_out.BackOutInteractor;
+import use_case.get_event_details.*;
+import use_case.join_event.JoinEventInteractor;
+import view.EventDetailsView;
+
+public class GetEventDetailsUseCaseFactory {
+    private GetEventDetailsUseCaseFactory(){}
+
+    public static EventDetailsView create(GetEventDetailsViewModel getEventDetailsViewModel, JoinEventController joinEventController,
+                                          BackOutController backOutController) {
+        return new EventDetailsView(getEventDetailsViewModel, joinEventController, backOutController);
+    }
+
+    public static GetEventDetailsController createGetEventDetailsUseCase(GetEventDetailsViewModel getEventDetailsViewModel,
+                                                                         ViewManagerModel viewManagerModel,
+                                                                         GetEventDetailsDataAccessInterface eventDataAccessObject) {
+        GetEventDetailsOutputBoundary presenter = new GetEventDetailsPresenter(getEventDetailsViewModel, viewManagerModel);
+        GetEventDetailsInputBoundary interactor = new GetEventDetailsInteractor(presenter, eventDataAccessObject);
+        return new GetEventDetailsController(interactor);
+
+    }
+}
