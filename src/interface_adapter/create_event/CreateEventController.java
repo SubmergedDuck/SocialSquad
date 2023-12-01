84bb738efa6d6e06920332f553dab7a2d59cf66e
Mikhael Orteza <mkorteza@gmail.com>
2023-11-30 23:04:32 -0500

    changed controller

--- src/interface_adapter/create_event/CreateEventController.java
+++ src/interface_adapter/create_event/CreateEventController.java
@@ -9,25 +9,9 @@ import java.io.IOException;
  * Controller for the create event use case.
  */
 public class CreateEventController {
-
-    private final String eventOwner;
-    private final String eventName;
-    private final String eventType;
-    private final String coordinates;
-    private final String date;
-    private final String capacity;
-    private final String description;
     private final CreateEventInputBoundary interactor;
 
-    public CreateEventController(String eventOwner, String eventName, String eventType, String coordinates, String capacity,
-                                 String description, String date, CreateEventInputBoundary interactor){
-        this.eventOwner = eventOwner;
-        this.eventName = eventName;
-        this.eventType = eventType;
-        this.coordinates = coordinates;
-        this.date = date;
-        this.capacity = capacity;
-        this.description = description;
+    public CreateEventController(CreateEventInputBoundary interactor){
         this.interactor = interactor;
     }
 
@@ -35,7 +19,8 @@ public class CreateEventController {
      * Provides the interactor with the input data.
      * @throws IOException api call error
      */
-    public void execute() throws IOException {
+    public void execute(String eventOwner, String eventName, String eventType, String coordinates, String capacity,
+                        String description, String date) throws IOException {
         CreateEventInputData inputData = new CreateEventInputData(eventOwner,eventName,coordinates,date,eventType,description,capacity);
         interactor.execute(inputData);
     }
