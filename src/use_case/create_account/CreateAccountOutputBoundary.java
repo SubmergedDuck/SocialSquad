4598cbb3e4d6debe2213f926d3c957594d30568b
Mikhael Orteza <mkorteza@gmail.com>
2023-12-02 23:46:55 -0500

    deleted unused and unimplemented use case

--- src/use_case/create_account/CreateAccountOutputBoundary.java
+++
@@ -1,6 +0,0 @@
-package use_case.create_account;
-
-public interface CreateAccountOutputBoundary {
-    void prepareSuccessView(CreateAccountOutputData user);
-    void prepareFailView(String error);
-}
