52ad156087e7ca837c51c1a9019e8aff064c7bd3
katrinasha <katrina.sha@mail.utoronto.ca>
2023-12-03 13:35:49 -0500

    Created MainFileTest.java.

---
+++ test/app/MainFileTest.java
@@ -0,0 +1,15 @@
+package app;
+
+import org.junit.Test;
+
+import java.io.IOException;
+
+import static org.junit.Assert.*;
+
+public class MainFileTest {
+
+    @Test
+    public void main() throws IOException {
+        MainFile.main(new String[]{});
+    }
+}
